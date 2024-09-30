76c89148f41676f69dac1fe5155f6dcb976a13e0
아노 <48114488+nukktae@users.noreply.github.com>
2024-09-30 14:34:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001; // Change from 3000 to 3001
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
